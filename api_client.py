34441c1f1e551cae56166deff7bf7d8838fe772f
selenasun1618 <selenas@stanford.edu>
2023-03-26 02:34:26 -0700

    first

---
+++ api_client.py
@@ -0,0 +1,38 @@
+import zmq
+from elevenlabslib import *
+import time as time_
+
+API_KEY = "2d876cea3d5fefadc247d37f6926daae"
+
+user = ElevenLabsUser(API_KEY)
+voice = user.get_voices_by_name("Rachel")[0]  # This is a list because multiple voices can have the same name
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to server…")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://192.168.1.100:5555")
+
+def stand_up():
+    send_request(b"stand")
+    time_.sleep(1)
+
+def sit_down():
+    send_request(b"sit")
+    time_.sleep(1)
+
+def command_velocity(x, yaw, time):
+    send_request(bytes("move_{}_{}_{}".format(x, yaw, time), "utf-8"))
+    time_.sleep(time)
+
+def send_request(request):
+    print(f"Sending request {request} …")
+    socket.send(request)
+
+    #  Get the reply.
+    message = socket.recv()
+    print(f"Received reply {request} [ {message} ]")
+
+def say(text):
+    voice.generate_and_play_audio(text, playInBackground=False)
---
+++ script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mv Audio* whisper_recording.mp3
+python3 whisper_test.py
